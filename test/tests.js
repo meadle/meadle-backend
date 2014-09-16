63a21cfd4a3ca2f29caabe965c39969157daf70d
Michael Hockerman <mike@hockerman.com>
2014-09-16 10:33:29 -0400

    Improved logging output during debugging.

--- test/tests.js
+++ test/tests.js
@@ -6,28 +6,17 @@ var app = require('../server');
 var request = require('supertest')
 var port = 3000;
 
-/** I just kind of use this to test code as im writing it. Nothing official and its cleaner than
- *  editing server.js every time. */
- /*
-exports.run = function() {
-  logger.trace("Running pre-start tests");
-
-
-  logger.trace("Pre-start tests completed successfully");
-}
-
-*/
-describe('POST /meeting', function(){
-  it('respond with json including the meadle id', postMeetingTest)
+describe('POST /meeting', function() {
+  it('POST /meeting should respond with a meeting ID', postMeetingTest)
 });
 
 
-function postMeetingTest(){
+function postMeetingTest() {
   request(app)
     .post('/meeting')
-    .send({userId:1234,lat:100.65,lng:60.75,datetime:2014-09-16})
+    .send({ 'userId':'1234','lat':100.65,'lng':60.75,'datetime':'2014-09-16' })
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect('meetingId');
+    .expect('meetingId')
     .expect(201);
 }
