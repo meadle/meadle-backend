8d2120d87567c2c70352575a87df2a1b1bc29612
Michael Hockerman <mike@hockerman.com>
2014-09-17 21:38:49 -0400

    Broke out mongo init functions to separate functions. Added callback to init definition for errors.

--- util/mongo_init.js
+++ util/mongo_init.js
@@ -3,42 +3,46 @@ var async = require("async");
 var db = require("mongoskin").db(process.env.MONGOLAB_URI || "mongodb://localhost:27017/meadle");
 var logger = require("log4js").getLogger();
 
-exports.init = function() {
+exports.init = function(callback) {
   logger.trace("mongo.init() : Initializing mongo database collections.");
 
-  async.parallel(
-
-    [
-      function(callback) {
-        db.createCollection("users", function(err, collection) {
-
-          if (err) {
-            logger.error("Error creating mongo users collection");
-          }
-          callback(err, collection);
-
-        });
-      },
-      function(callback) {
-        db.createCollection("meetings", function(err, collection) {
-
-          if (err) {
-            logger.error("Error creating mongo meetings collection");
-          }
-          callback(err, collection);
-
-        });
-      }
-    ],
+  async.parallel( [ initUsers, initMeetings ],
 
     function(err, results) {
 
       if (err) {
         logger.fatal("Error during mongo initialization.");
       }
+      callback(err, results);
 
     }
 
   )
+
+}
+
+var initUsers = function(callback) {
+
+  db.createCollection("users", function(err, collection) {
+
+    if (err) {
+      logger.error("Error creating mongo users collection");
+    }
+    callback(err, collection);
+
+  });
+
+}
+
+var initMeetings = function(callback) {
+
+  db.createCollection("meetings", function(err, collection) {
+
+    if (err) {
+      logger.error("Error creating mongo meetings collection");
+    }
+    callback(err, collection);
+
+  });
 
 }
