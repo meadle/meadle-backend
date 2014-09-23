f4133d4ab20ad1b87bdabab7a774b454abf579d1
Michael Hockerman <mike@hockerman.com>
2014-09-23 16:32:37 -0400

    Modified join route to store object of top locations instead of array

--- routes/join_meeting.js
+++ routes/join_meeting.js
@@ -143,8 +143,14 @@ var onGetYelpBusinesses = function(response, meetingId, userId) {
       }
     }
 
-    // Results should be a list of business IDs. Store them in mongo.
-    mongoMeetings.setTopLocations(meetingId, results, onTopLocationsSet(response, meetingId, userId))
+    // Results should be a list of business IDs.
+    // Create an object such that each id is a key in the object, then store that object in mongo
+    // The value for the key (business id) is the number of votes that business currently has (0)
+    var finalObject = {}
+    results.forEach(function(item)) {
+      finalObject.item = 0
+    }
+    mongoMeetings.setTopLocations(meetingId, finalObjects, onTopLocationsSet(response, meetingId, userId))
 
   }
 
