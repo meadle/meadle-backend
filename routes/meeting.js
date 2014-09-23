e3cc52db7d3f412f032a1e767637f8c0ad2c01d6
Michael Hockerman <mike@hockerman.com>
2014-09-23 14:29:37 -0400

    Moved create meeting function to its own file and updated server.js

--- routes/meeting.js
+++ routes/meeting.js
@@ -7,40 +7,6 @@ var mongoUsers = require("../util/mongo_users");
 var mongoMeetings = require("../util/mongo_meetings");
 var yelp = require("../util/yelp");
 
-exports.postMeeting = function(req, res) {
-
-	// Extract data from the post data
-	var me = req.body.userId;
-	var lat = req.body.lat;
-	var lng = req.body.lng;
-	var datetime = req.body.datetime;
-
-	if (!me || !lat || !lng || !datetime) {
-		logger.warn("Client supplied an illformatted POST body. Sending 400.");
-		res.status(400).send({"error":400, "message": "POST body was not formatted correctly."}); return;
-	}
-
-	// Generate a random meeting id
-	var mid = Math.random().toString(36).substring(5);
-
-	// Create the user in mongo
-	mongoUsers.createUser({"userId": me, "lat": lat, "lng": lng});
-
-	// Create the meeting
-	mongoMeetings.createMeeting({"meetingId": mid, "datetime": datetime, "members": [me]}, function(err, result) {
-
-		if (err) {
-			logger.error("Error while inserting meeting into mongo. Returning 500.")
-			res.status(500).send({"error":500, "message":"Internal server error"}); return;
-		}
-
-		// Pass back the meeting id to the client
-		res.status(201).send({"meetingId": mid});
-
-	});
-
-}
-
 exports.joinMeeting = function(req, res) {
 
 	// Extract from post data
