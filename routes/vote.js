6cd9f4e19ffebe3ceaef4a1240cf6b1808d1d0f1
Michael Hockerman <mike@hockerman.com>
2014-09-23 16:24:24 -0400

    Added voting js

---
+++ routes/vote.js
@@ -0,0 +1,10 @@
+
+var logger = require("log4js").getLogger()
+var mongoMeetings = require("../util/mongo_meetings")
+var mongoUsers = require("../util/mongo_users")
+
+module.exports = function(req, res) {
+
+  
+
+}
