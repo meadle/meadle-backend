185675ef8d8e3ac682c99dc1d201de6f75f7a397
Eric Templin <ertemplin@gmail.com>
2014-09-24 21:45:29 -0400

    Add basic GMC test and make it pass

--- util/gcm.js
+++ util/gcm.js
@@ -1,5 +1,6 @@
 var logger = require('log4js').getLogger();
 var request = require('request');
+var Q = require('q');
 
 module.exports = function(gcmKey) {
 	return {
@@ -13,9 +14,19 @@ module.exports = function(gcmKey) {
 				'data'				: data,
 			};
 
-			request.post({url: 'https://android.googleapis.com/gcm/send', body: requestBody, json:true}, function(err, msg, body) {
-				// TODO: Handle response
+			var promise = Q.defer();
+
+			request.post({url: 'https://android.googleapis.com/gcm/send', body: requestBody, json:true, headers: { 'Authorization': 'key=' + gcmKey, 'Content-Type': 'application/json' }}, function(err, msg, body) {
+				if(err) {
+					// TODO: If error, then retry after time that we are supposed to wait. 
+					// TODO: If registration_ids error, then raise error
+					promise.reject(msg);
+				} else {
+					promise.resolve(msg);	
+				}
 			});
+
+			return promise.promise;
 		}
 	}
 };
