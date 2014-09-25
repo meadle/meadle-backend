a19cdbcc68de54047542696a2ecfb420136f9d18
Eric Templin <ertemplin@gmail.com>
2014-09-25 17:01:29 -0400

    Implement basic error handling in GCM

--- util/gcm.js
+++ util/gcm.js
@@ -16,15 +16,40 @@ module.exports = function(gcmKey) {
 
 			var promise = Q.defer();
 
-			request.post({url: 'https://android.googleapis.com/gcm/send', body: requestBody, json:true, headers: { 'Authorization': 'key=' + gcmKey, 'Content-Type': 'application/json' }}, function(err, msg, body) {
+			var requestOptions = {
+				url: 'https://android.googleapis.com/gcm/send', 
+				body: requestBody, 
+				json: true, 
+				headers: { 
+					'Authorization': 'key=' + gcmKey, 
+					'Content-Type': 'application/json' 
+				
+				}
+			};
+
+			function doGcmRequest(requestOptions) {
+				request.post(requestOptions, handleGcmResponse);
+			}
+
+			function handleGcmResponse(err, msg, body) {
 				if(err) {
-					// TODO: If error, then retry after time that we are supposed to wait. 
 					// TODO: If registration_ids error, then raise error
+					if(msg.statusCode >= 500 && msg.statusCode <= 599) {
+						// Errors in the 500-599 range (such as 500 or 503) indicate that 
+						// there was an internal error in the GCM server while trying to 
+						// process the request, or that the server is temporarily unavailable 
+						// (for example, because of timeouts). Sender must retry later, honoring 
+						// any Retry-After header included in the response.
+						setTimeout(msg.getHeader('Retry-After'), doGcmRequest(requestOptions));
+						return;
+					}
 					promise.reject(msg);
 				} else {
 					promise.resolve(msg);	
 				}
-			});
+			}
+
+			doGcmRequest(requestOptions);
 
 			return promise.promise;
 		}
