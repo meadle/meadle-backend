ab0bad597a4e3603fef09806896b4685935968db
Michael Hockerman <mike@hockerman.com>
2014-09-25 15:05:04 -0400

    Added it to post object.

--- routes/create_meeting.js
+++ routes/create_meeting.js
@@ -8,6 +8,7 @@ module.exports = function(req, res) {
 
   // Extract data from the post data
   var me = req.body.userId
+  var gcm = req.body.gcm
   var lat = req.body.lat
   var lng = req.body.lng
   var datetime = req.body.datetime
@@ -20,7 +21,7 @@ module.exports = function(req, res) {
   var mid = Math.random().toString(36).substring(5)
 
   // Create the user in mogno
-  var user = {"userId": me, "lat": lat, "lng": lng}
+  var user = {"userId": me, "gcm", gcm, "lat": lat, "lng": lng}
   mongoUsers.createUser(user, onMongoUserCreated(res, mid, datetime, me))
 
 }
