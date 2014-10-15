8b5c78e9d52a6e43b1493ea5675852505c174b5f
Mike Hockerman <mike@hockerman.com>
2014-10-15 13:57:55 -0400

    Implemented create meeting

--- firebase/meetings.js
+++ firebase/meetings.js
@@ -1,3 +1,30 @@
 
 var fbr = require('firebase')
-var fb  = new fbr('https://meadle.firebaseio.com')
+var fbo = new fbr('https://meadle.firebaseio.com')
+var fb  = fbo.child('meetings')
+var logger = require('log4js').getLogger()
+
+/** Creates a new meeting on firebase */
+exports.createMeeting = function(meeting, callback) {
+
+  // Filter out only the fields we want to store in firebase
+  var nmeeting = {}
+  nmeeting.datetime = meeting.datetime
+  nmeeting.members = meeting.members
+  nmeeting.nVoted = meeting.nVoted
+
+  // Make sure all the fields are set with non-null values
+  if (!nmeeting.datetime || !nmeeting.members || !nmeeting.nVoted) {
+    logger.error("Attempting to create incomplete meeting in firebase. Rejecting push.")
+    return
+  }
+
+  // Set the meeting information into firebase
+  fb.child(meeting.meetingId).set(nmeeting, function(err) {
+    if (err) {
+      logger.error("Error setting object in firebase")
+      logger.error(err)
+    }
+  })
+
+}
